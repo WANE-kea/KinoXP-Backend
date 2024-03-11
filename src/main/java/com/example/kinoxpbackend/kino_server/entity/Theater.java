a75ff05a94e6dcc30291ebb250b627104771a805
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2024-03-11 14:47:02 +0100

    Nikolaj prøver på at lære Java

--- src/main/java/com/example/kinoxpbackend/kino_server/entity/Theater.java
+++ src/main/java/com/example/kinoxpbackend/kino_server/entity/Theater.java
@@ -6,8 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Set;
-
+import java.util.List;
 
 @Getter
 @Setter
@@ -22,14 +21,9 @@ public class Theater {
     @Column(unique = true)
     private String name;
 
-    @OneToMany(mappedBy = "theaters", cascade = CascadeType.PERSIST)
-    private Set<Theater> theaters;
-    public void addTheater(Theater theater) {
-        this.theaters.add(theater);
-        theater.setTheater(this);
-    }
-    public Theater(String name) {
-        this.name = name;
+    @OneToMany
+    private List<Seat> seats;
 
-    }
+    @OneToMany
+    private List<Show> shows;
 }
