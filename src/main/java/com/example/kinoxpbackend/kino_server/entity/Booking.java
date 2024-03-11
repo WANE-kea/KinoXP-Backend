b1f58b1593bbe6405bc3292c91fe8f7428bdca51
Blue-Grizzly <emilnissen2@gmail.com>
2024-03-11 11:11:09 +0100

    Created Booking entity with JoinTable for seats

--- src/main/java/com/example/kinoxpbackend/kino_server/entity/Booking.java
+++ src/main/java/com/example/kinoxpbackend/kino_server/entity/Booking.java
@@ -1,4 +1,36 @@
 package com.example.kinoxpbackend.kino_server.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+
+@Table(name = "bookings")
 public class Booking {
+    @Id
+    private int id;
+    private int customerId;
+    private int showId;
+    private int bookingNr;
+
+    @JoinTable(
+            name = "seat_booking",
+            joinColumns = @JoinColumn(
+                    name = "seat_id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "booking"
+            )
+    )
+    @OneToMany
+    private List<Seat> seats;
+
 }
