fdf79787feb13951aaa43993251cae7e9711aa18
Blue-Grizzly <emilnissen2@gmail.com>
2024-03-11 15:05:50 +0100

    Test

--- src/main/java/com/example/kinoxpbackend/kino_server/entity/Show.java
+++ src/main/java/com/example/kinoxpbackend/kino_server/entity/Show.java
@@ -1,12 +1,13 @@
 package com.example.kinoxpbackend.kino_server.entity;
 
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
+
+//Test
 
 @Getter
 @Setter
