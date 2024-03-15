3173fe36e4a4fbd87e5697f5612b669f24952e5e
Blue-Grizzly <emilnissen2@gmail.com>
2024-03-15 13:22:07 +0100

    fixed infinite recursion and disallowed data type i think?

--- src/main/java/com/example/kinoxpbackend/kino_server/dto/MovieDto.java
+++ src/main/java/com/example/kinoxpbackend/kino_server/dto/MovieDto.java
@@ -1,10 +1,13 @@
 package com.example.kinoxpbackend.kino_server.dto;
 
+import com.example.kinoxpbackend.kino_server.entity.Category;
 import com.example.kinoxpbackend.kino_server.entity.Movie;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -13,10 +16,12 @@ import lombok.Setter;
 public class MovieDto {
     private Integer id;
     private String title;
+    private List<Category> categories;
 
     public MovieDto(Movie c) {
         this.id = c.getId();
         this.title = c.getTitle();
+        this.categories = c.getCategories();
 
     }
 }
